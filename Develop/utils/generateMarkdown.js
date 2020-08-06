51e7645b1aed686cd40504b4376e705673faabb4
Julie Schaub <julie.schaub@hotmail.com>
2020-08-06 18:01:19 -0500

    updated files

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,6 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `# ${answers.title}
 
 `;
 }
